55afc04fc7b54c61d6f5d280a9a75ec8ad268ed9
BoeunHan <h.boeunn@gmail.com>
2025-08-04 22:34:51 +0900

    chore: lucide react 라이브러리 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { ChattingButton } from "@/components/ChattingButton";
+
 export default function Home() {
-  return <div>안녕하세요.</div>;
+  return (
+    <div>
+      안녕하세요.
+      <div className="fixed bottom-12 left-12">
+        <ChattingButton />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ChattingButton.tsx
@@ -0,0 +1,9 @@
+import { MessageSquareMoreIcon } from "lucide-react";
+
+export const ChattingButton = () => {
+  return (
+    <button className="rounded-full shadow-lg p-4 bg-white cursor-pointer">
+      <MessageSquareMoreIcon />
+    </button>
+  );
+};
