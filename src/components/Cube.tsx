35337bead83bc16f225a3c9b1eaa1b0e10eb8e68
BoeunHan <h.boeunn@gmail.com>
2025-08-07 18:31:46 +0900

    feat: 조명 추가

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -13,26 +13,32 @@ export const Cube = () => {
     const width = container.clientWidth;
     const height = container.clientHeight;
 
-    // 1. 씬(Scene) 만들기
+    // 씬(Scene) 만들기
     const scene = new THREE.Scene();
 
-    // 2. 카메라(Camera) 만들기
+    // 카메라(Camera) 만들기
     const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 2000);
     camera.position.z = 5;
 
-    // 3. 렌더러(Renderer) 만들기
+    // 렌더러(Renderer) 만들기
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(width, height);
     renderer.setClearColor(0xfafaf8);
     container.appendChild(renderer.domElement);
 
-    // 4. 큐브(박스) 만들기
+    // 큐브(박스) 만들기
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffc4b7 });
+    const material = new THREE.MeshLambertMaterial({ color: 0xffc4b7 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    // 5. 애니메이션 루프 만들기
+    // 조명 추가하기
+    const light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(5, 5, 5);
+    scene.add(light);
+    scene.add(new THREE.AmbientLight(0xffffff, 1));
+
+    // 애니메이션 루프 만들기
     const animate = () => {
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
@@ -52,7 +58,7 @@ export const Cube = () => {
 
     window.addEventListener("resize", handleResize);
 
-    // 6. 클린업 함수 (컴포넌트 언마운트 시 정리)
+    // 클린업 함수 (컴포넌트 언마운트 시 정리)
     return () => {
       renderer.dispose();
       container?.removeChild(renderer.domElement);
