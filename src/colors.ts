78d81ae4688a4f5fd56504cca8945a74147c56e8
BoeunHan <h.boeunn@gmail.com>
2025-08-09 16:19:35 +0900

    feat: 면마다 색 3*3 배열로 저장

--- src/colors.ts
+++ src/colors.ts
@@ -1,9 +1,42 @@
-export const COLORS = {
-  R: 0xffc4b7,
-  L: 0x93cef6,
-  U: 0xd0f8b8,
-  D: 0xffe7c0,
-  F: 0xffd1a6,
-  B: 0xfafaf8,
-  NONE: 0xaeaeae,
+export enum Color {
+  RED = 0xffc4b7,
+  ORANGE = 0xffd1a6,
+  YELLOW = 0xffe7c0,
+  GREEN = 0xd0f8b8,
+  BLUE = 0x93cef6,
+  WHITE = 0xfafaf8,
+  NONE = 0xaeaeae,
+}
+
+export const CUBE_COLORS = {
+  R: [
+    [Color.RED, Color.RED, Color.RED],
+    [Color.RED, Color.RED, Color.RED],
+    [Color.RED, Color.RED, Color.RED],
+  ],
+  L: [
+    [Color.BLUE, Color.BLUE, Color.BLUE],
+    [Color.BLUE, Color.BLUE, Color.BLUE],
+    [Color.BLUE, Color.BLUE, Color.BLUE],
+  ],
+  U: [
+    [Color.GREEN, Color.GREEN, Color.GREEN],
+    [Color.GREEN, Color.GREEN, Color.GREEN],
+    [Color.GREEN, Color.GREEN, Color.GREEN],
+  ],
+  D: [
+    [Color.YELLOW, Color.YELLOW, Color.YELLOW],
+    [Color.YELLOW, Color.YELLOW, Color.YELLOW],
+    [Color.YELLOW, Color.YELLOW, Color.YELLOW],
+  ],
+  F: [
+    [Color.ORANGE, Color.ORANGE, Color.ORANGE],
+    [Color.ORANGE, Color.ORANGE, Color.ORANGE],
+    [Color.ORANGE, Color.ORANGE, Color.ORANGE],
+  ],
+  B: [
+    [Color.WHITE, Color.WHITE, Color.WHITE],
+    [Color.WHITE, Color.WHITE, Color.WHITE],
+    [Color.WHITE, Color.WHITE, Color.WHITE],
+  ],
 };
