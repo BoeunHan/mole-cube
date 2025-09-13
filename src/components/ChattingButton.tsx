41eb3acccca2dc089df41ae751422f579d515e26
BoeunHan <h.boeunn@gmail.com>
2025-09-13 18:05:38 +0900

    feat: 회전 history popover 구현

--- src/components/ChattingButton.tsx
+++ src/components/ChattingButton.tsx
@@ -1,9 +1,51 @@
+"use client";
+
 import { MessageSquareMoreIcon } from "lucide-react";
+import { Popover, PopoverContent } from "@/components/ui/popover";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { useGameSocket } from "@/providers/GameSocketContext";
+import { useState } from "react";
 
 export const ChattingButton = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <button className="rounded-full shadow-lg p-4 bg-white cursor-pointer">
-      <MessageSquareMoreIcon />
-    </button>
+    <Popover open={open}>
+      <PopoverTrigger asChild>
+        <button
+          className="rounded-full shadow-lg p-4 bg-white cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <MessageSquareMoreIcon />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent
+        align="start"
+        side="top"
+        className="w-80 bg-black/40"
+        onPointerDownOutside={(e) => {
+          e.preventDefault(); // 외부 클릭 무시
+        }}
+      >
+        <HistoryContent />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+const HistoryContent = () => {
+  const { histories } = useGameSocket();
+
+  return (
+    <div>
+      {histories.map(({ userId, timestamp, face, clockwise }) => (
+        <div key={timestamp}>
+          <div>{userId}</div>
+          <div>{timestamp}</div>
+          <div>{face}</div>
+          <div>{clockwise}</div>
+        </div>
+      ))}
+    </div>
   );
 };
